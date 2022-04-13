938af8e1501467299e5e78d77cff1e302d9f0eab
jackets82 <ad_coates@hotmail.com>
2022-04-13 11:42:11 +0100

    burger menu

--- components/Navbar.js
+++ components/Navbar.js
@@ -15,6 +15,16 @@ function Navbar() {
             />
           </Link>
         </div>
+        <div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+          >
+            <path d="M24 6h-24v-4h24v4zm0 4h-24v4h24v-4zm0 8h-24v4h24v-4z" />
+          </svg>
+        </div>
         <div className=" md:flex gap-5 text-sm tracking-wider hidden">
           <NavbarButtons text="HOME" link="/" />
           <NavbarButtons text="ABOUT" link="/about" />
