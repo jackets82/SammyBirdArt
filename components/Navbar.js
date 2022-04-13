e3a920b1cb226e03c81e29de5e5f21eaf4270f86
jackets82 <ad_coates@hotmail.com>
2022-04-13 13:10:53 +0100

    burger finished

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,6 @@
 import { Menu, Transition } from "@headlessui/react";
 import Link from "next/link";
-import { React, useState } from "react";
+import { React, useState, Fragment } from "react";
 import NavbarButtons from "./NavbarButtons";
 
 function Navbar() {
@@ -10,7 +10,7 @@ function Navbar() {
   };
 
   return (
-    <div className=" sticky top-0 bg-white z-50">
+    <div className=" sticky top-0 bg-white z-50 shadow-lg">
       <div className=" max-w-5xl mx-auto flex justify-between h-40 items-center sticky top-0 z-50 bg-white pr-5 lg:pr-0">
         <div>
           <Link href="/">
@@ -21,7 +21,7 @@ function Navbar() {
             />
           </Link>
         </div>
-        <div className=" md:hidden pr-5 cursor-pointer">
+        <div className="md:hidden pr-5 cursor-pointer">
           <Menu>
             <Menu.Button>
               <svg
@@ -33,12 +33,21 @@ function Navbar() {
                 <path d="M24 6h-24v-4h24v4zm0 4h-24v4h24v-4zm0 8h-24v4h24v-4z" />
               </svg>
             </Menu.Button>
+
             <Menu.Items>
-              <div className=" absolute -bottom-[185px] w-48 rounded bg-white p-3 z-50 flex flex-col right-1 gap-5">
-                <NavbarButtons text="HOME" link="/" />
-                <NavbarButtons text="ABOUT" link="/about" />
-                <NavbarButtons text="GALLERY" link="/gallery" />
-                <NavbarButtons text="CONTACT" link="/" />
+              <div className=" absolute -bottom-[185px] w-48 rounded bg-white p-3 z-50 flex flex-col right-1 gap-5 origin-top-right">
+                <div className=" hover:bg-sammyBirdGray hover:-m-2 hover:p-2 duration-50 transition-all rounded">
+                  <NavbarButtons text="HOME" link="/" />
+                </div>
+                <div className=" hover:bg-sammyBirdGray hover:-m-2 hover:p-2 duration-50 transition-all rounded">
+                  <NavbarButtons text="ABOUT" link="/about" />
+                </div>
+                <div className=" hover:bg-sammyBirdGray hover:-m-2 hover:p-2 duration-50 transition-all rounded">
+                  <NavbarButtons text="GALLERY" link="/gallery" />
+                </div>
+                <div className=" hover:bg-sammyBirdGray hover:-m-2 hover:p-2 duration-50 transition-all rounded">
+                  <NavbarButtons text="CONTACT" link="/" />
+                </div>
               </div>
             </Menu.Items>
           </Menu>
